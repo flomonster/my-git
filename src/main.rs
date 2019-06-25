fd46ca08df06c8b54b7454495b60c55756665ee4
Florian Amsallem <florian.amsallem@epita.fr>
2019-06-25 17:27:21 +0200

    init: add command

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 #[macro_use]
 extern crate clap;
 use clap::App;
+use std::process::exit;
 
 fn main() {
     let yaml = load_yaml!("cli.yml");
     let mut app = App::from_yaml(yaml);
-    my_git::run(&mut app);
+    if let Err(e) = my_git::run(&mut app) {
+        eprintln!("{}", e);
+        exit(2);
+    }
 }
