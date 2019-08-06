1cd5f3f8d514c3639836590a1de2f69a3fef0038
Florian Amsallem <florian.amsallem@epita.fr>
2019-08-06 17:44:53 +0200

    commit: add config loading

--- src/commit.rs
+++ src/commit.rs
@@ -1,3 +1,4 @@
+use crate::config::{Config, ConfigError};
 use crate::index::Index;
 use crate::objects::Tree;
 use crate::utils;
@@ -6,6 +7,17 @@ use std::error::Error;
 
 pub fn run(_args: &ArgMatches) -> Result<(), Box<dyn Error>> {
     // TODO: Check that files has been added
+
+    // Load config
+    let config = Config::load()?;
+    let user_name = config.user.name;
+    let user_email = config.user.email;
+
+    // Return an error in case of an empty configuration
+    match &user_name {
+        Some(name) if name != &String::new() => (),
+        _ => return Err(Box::new(ConfigError::MissingAuthor(user_email))),
+    }
 
     // Create tree object
     let repo_path = utils::find_repo()?;
