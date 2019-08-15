101daa9b775a5070949605a7f1e6d700135f1b41
Florian Amsallem <florian.amsallem@epita.fr>
2019-08-15 04:46:23 +0200

    branch: add display

--- src/cmd/branch.rs
+++ src/cmd/branch.rs
@@ -1,6 +1,31 @@
+use crate::{refs, utils};
 use clap::ArgMatches;
+use colored::Colorize;
 use std::error::Error;
+use std::path::PathBuf;
 
-pub fn run(args: &ArgMatches) -> Result<(), Box<dyn Error>> {
+fn display(repo_path: &PathBuf) {
+    let branches = refs::branches(&repo_path);
+    let current_branch = match refs::current_branch(&repo_path) {
+        Some((branch, _)) => branch,
+        _ => String::new(),
+    };
+
+    for (branch, _) in branches {
+        if current_branch == branch {
+            println!("* {}", branch.green());
+        } else {
+            println!("  {}", branch);
+        }
+    }
+}
+
+pub fn run(_args: &ArgMatches) -> Result<(), Box<dyn Error>> {
+    let repo_path = utils::find_repo()?;
+
+    // TODO: Check branchname arg and delete option
+
+    display(&repo_path);
+
     Ok(())
 }
